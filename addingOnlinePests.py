7d380f59b98756df155809098be355413b50b103
laur <lv70xo@gmail.com>
2017-08-29 15:44:42 +0200

    Update addingOnlinePests.py

--- addingOnlinePests.py
+++ addingOnlinePests.py
@@ -1,17 +1,10 @@
 from Bio import SeqIO
 
-#original_file = "./both_derep.fas"
-#corrected_file = "./corrected_derep.fas"
 original_file ="./Arthropoda_EU_NS.fas"
 corrected_file="./Arthropoda_wPST.fas"
 
 with open("pests_from_online", 'r') as pests_online:
     speclist = pests_online.read().splitlines()
-
-#pests_online = open("pests_from_online", 'r')
-#speclist = pests_online.readlines()
-#there are a few empty strings still in it, though.
-#speclist=["cockatiel","milk","bread"]
 
 
 with open(original_file) as original, open(corrected_file, 'w') as corrected:
@@ -20,9 +13,8 @@ with open(original_file) as original, open(corrected_file, 'w') as corrected:
     for record in records:
         if any([record.id.__contains__(sp) for sp in speclist]):
             record.id += "|PEST"
-            print(record.id)
+            record.description += "|PEST"
             counter +=1
-        else:
-            print("false")
+        SeqIO.write(record, corrected, 'fasta')
     print(counter)
-
+    
